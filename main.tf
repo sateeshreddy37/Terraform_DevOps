d8af2abf1d11e95f040f67c5f304317c83109438
Sateesh Reddy Panga <134610149+sateeshreddy37@users.noreply.github.com>
2023-07-19 12:29:50 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "sateeshrg" {
-  location = "eastus"
-  name     = "sateesh-rg"
+resource "azurerm_resource_group" "nagrg" {
+  location = var.location
+  name     = "nag-rg"
 }
 
-resource "azurerm_virtual_network" "sateeshvnet" {
-  name                = "sateesh-vnet"
-  resource_group_name = "sateesh-rg"
-  address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
+resource "azurerm_virtual_network" "nagvnet" {
+  name                = "nag-vnet"
+  resource_group_name = "nag-rg"
+  address_space       = var.vnet-range
+  location            = var.location
   #depends_on = [
     #azurerm_resource_group.ntierrg
   #]
